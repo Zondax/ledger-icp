162da7618602b14e982272cc6b8ef5a8d4bad744
Juan Leni <juan.leni@zondax.ch>
2021-03-17 09:21:07 +0100

    update dependencies

--- tests_zemu/tests/test.js
+++ tests_zemu/tests/test.js
@@ -159,7 +159,7 @@ describe('Standard', function () {
             // Wait until we are not in the main menu
             await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot());
 
-            await sim.compareSnapshotsAndAccept(".", `${prefix.toLowerCase()}-sign_basic_normal`, 8);
+            await sim.compareSnapshotsAndAccept(".", `${prefix.toLowerCase()}-sign_basic_normal`, model === "nanos" ? 8 : 9);
 
             let signatureResponse = await respRequest;
             console.log(signatureResponse);
