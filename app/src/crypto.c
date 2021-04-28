99c15d7b466ae062f6072714431e2121b04cb1ac
Leon Groot Bruinderink <lgrootbr@gmail.com>
2021-04-28 17:21:10 +0200

    fixes to cpp tests

--- app/src/crypto.c
+++ app/src/crypto.c
@@ -300,6 +300,9 @@ zxerr_t crypto_sign(uint8_t *signatureBuffer,
 #include "picohash.h"
 
 zxerr_t crypto_extractPublicKey(const uint32_t path[HDPATH_LEN_DEFAULT], uint8_t *pubKey, uint16_t pubKeyLen) {
+    const char *tmp = "0410d34980a51af89d3331ad5fa80fe30d8868ad87526460b3b3e15596ee58e812422987d8589ba61098264df5bb9c2d3ff6fe061746b4b31a44ec26636632b835";
+    parseHexString(pubKey, pubKeyLen, tmp);
+
     return zxerr_ok;
 }
 
