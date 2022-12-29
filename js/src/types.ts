65fbc47efa9964310e81449ed14fb661ff13ce6b
Carlo Sala <carlosalag@protonmail.com>
2022-12-29 12:16:55 +0100

    make use of @zondax/ledger-js package

--- js/src/types.ts
+++ js/src/types.ts
@@ -1,53 +1,28 @@
-import { LedgerError } from './common'
+import { INSGeneric, ResponseBase } from "@zondax/ledger-js";
 
-export interface ResponseBase {
-  errorMessage?: string
-  returnCode: LedgerError
+export interface ICPIns extends INSGeneric {
+  GET_VERSION: 0x00;
+  GET_ADDR_SECP256K1: 0x01;
+  SIGN_SECP256K1: 0x02;
+  SIGN_COMBINED: 0x03;
 }
 
 export interface ResponseAddress extends ResponseBase {
-  publicKey: Buffer
-  principal: Buffer
-  address: Buffer
-  principalText: string
-}
-
-export interface ResponseVersion extends ResponseBase {
-  testMode: boolean
-  major: number
-  minor: number
-  patch: number
-  deviceLocked: boolean
-  targetId: string
-}
-
-export interface ResponseAppInfo extends ResponseBase {
-  appName: string
-  appVersion: string
-  flagLen: number
-  flagsValue: number
-  flagRecovery: boolean
-  flagSignedMcuCode: boolean
-  flagOnboarded: boolean
-  flagPINValidated: boolean
-}
-
-export interface ResponseDeviceInfo extends ResponseBase {
-  targetId: string
-  seVersion: string
-  flag: string
-  mcuVersion: string
+  publicKey?: Buffer;
+  principal?: Buffer;
+  address?: Buffer;
+  principalText?: string;
 }
 
 export interface ResponseSign extends ResponseBase {
-  preSignHash: Buffer
-  signatureRS: Buffer
-  signatureDER: Buffer
+  preSignHash?: Buffer;
+  signatureRS?: Buffer;
+  signatureDER?: Buffer;
 }
 
 export interface ResponseSignUpdateCall extends ResponseBase {
-  RequestHash: Buffer
-  RequestSignatureRS: Buffer
-  StatusReadHash: Buffer
-  StatusReadSignatureRS: Buffer
+  RequestHash?: Buffer;
+  RequestSignatureRS?: Buffer;
+  StatusReadHash?: Buffer;
+  StatusReadSignatureRS?: Buffer;
 }
