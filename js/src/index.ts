ebb464d86ef1d2f8f3a1eccd6d0bf019c7fbf059
Carlo Sala <carlosalag@protonmail.com>
2022-12-29 10:25:15 +0100

    run prettier

--- js/src/index.ts
+++ js/src/index.ts
@@ -14,15 +14,8 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  ******************************************************************************* */
-import Transport from '@ledgerhq/hw-transport';
-import {
-  ResponseAddress,
-  ResponseAppInfo,
-  ResponseDeviceInfo,
-  ResponseSign,
-  ResponseSignUpdateCall,
-  ResponseVersion
-} from './types';
+import Transport from '@ledgerhq/hw-transport'
+import { ResponseAddress, ResponseAppInfo, ResponseDeviceInfo, ResponseSign, ResponseSignUpdateCall, ResponseVersion } from './types'
 import {
   ADDRLEN,
   CHUNK_SIZE,
@@ -39,29 +32,31 @@ import {
   processErrorResponse,
   serializePath,
   SIGRSLEN,
-} from './common';
+} from './common'
 
-export {LedgerError};
-export * from './types';
+export { LedgerError }
+export * from './types'
 
 function processGetAddrResponse(response: Buffer) {
-  let partialResponse = response;
+  let partialResponse = response
 
-  const errorCodeData = partialResponse.slice(-2);
-  const returnCode = (errorCodeData[0] * 256 + errorCodeData[1]);
+  const errorCodeData = partialResponse.slice(-2)
+  const returnCode = errorCodeData[0] * 256 + errorCodeData[1]
 
-  const publicKey = Buffer.from(partialResponse.slice(0, PKLEN));
-  partialResponse = partialResponse.slice(PKLEN);
+  const publicKey = Buffer.from(partialResponse.slice(0, PKLEN))
+  partialResponse = partialResponse.slice(PKLEN)
 
-  const principal = Buffer.from(partialResponse.slice(0, PRINCIPAL_LEN));
+  const principal = Buffer.from(partialResponse.slice(0, PRINCIPAL_LEN))
 
-  partialResponse = partialResponse.slice(PRINCIPAL_LEN);
+  partialResponse = partialResponse.slice(PRINCIPAL_LEN)
 
-  const address = Buffer.from(partialResponse.slice(0, ADDRLEN));
+  const address = Buffer.from(partialResponse.slice(0, ADDRLEN))
 
-  partialResponse = partialResponse.slice(ADDRLEN);
+  partialResponse = partialResponse.slice(ADDRLEN)
 
-  const principalText = Buffer.from(partialResponse.slice(0, -2)).toString().replace(/(.{5})/g, "$1-");
+  const principalText = Buffer.from(partialResponse.slice(0, -2))
+    .toString()
+    .replace(/(.{5})/g, '$1-')
 
   return {
     publicKey,
@@ -70,75 +65,75 @@ function processGetAddrResponse(response: Buffer) {
     principalText,
     returnCode,
     errorMessage: errorCodeToString(returnCode),
-  };
+  }
 }
 
 export default class InternetComputerApp {
-  private transport: Transport;
+  private transport: Transport
 
   constructor(transport: Transport) {
     if (!transport) {
-      throw new Error('Transport has not been defined');
+      throw new Error('Transport has not been defined')
     }
     this.transport = transport
   }
 
   static prepareChunks(serializedPathBuffer: Buffer, message: Buffer) {
-    const chunks = [];
+    const chunks = []
 
     // First chunk (only path)
-    chunks.push(serializedPathBuffer);
+    chunks.push(serializedPathBuffer)
 
-    const messageBuffer = Buffer.from(message);
+    const messageBuffer = Buffer.from(message)
 
-    const buffer = Buffer.concat([messageBuffer]);
+    const buffer = Buffer.concat([messageBuffer])
     for (let i = 0; i < buffer.length; i += CHUNK_SIZE) {
-      let end = i + CHUNK_SIZE;
+      let end = i + CHUNK_SIZE
       if (i > buffer.length) {
-        end = buffer.length;
+        end = buffer.length
       }
-      chunks.push(buffer.slice(i, end));
+      chunks.push(buffer.slice(i, end))
     }
 
-    return chunks;
+    return chunks
   }
 
   async signGetChunks(path: string, message: Buffer) {
-    return InternetComputerApp.prepareChunks(serializePath(path), message);
+    return InternetComputerApp.prepareChunks(serializePath(path), message)
   }
 
   async getVersion(): Promise<ResponseVersion> {
-    return getVersion(this.transport).catch(err => processErrorResponse(err));
+    return getVersion(this.transport).catch(err => processErrorResponse(err))
   }
 
   async getAppInfo(): Promise<ResponseAppInfo> {
     return this.transport.send(0xb0, 0x01, 0, 0).then(response => {
-      const errorCodeData = response.slice(-2);
-      const returnCode = errorCodeData[0] * 256 + errorCodeData[1];
+      const errorCodeData = response.slice(-2)
+      const returnCode = errorCodeData[0] * 256 + errorCodeData[1]
 
-      const result: { errorMessage?: string; returnCode?: LedgerError } = {};
+      const result: { errorMessage?: string; returnCode?: LedgerError } = {}
 
-      let appName = 'err';
-      let appVersion = 'err';
-      let flagLen = 0;
-      let flagsValue = 0;
+      let appName = 'err'
+      let appVersion = 'err'
+      let flagLen = 0
+      let flagsValue = 0
 
       if (response[0] !== 1) {
         // Ledger responds with format ID 1. There is no spec for any format != 1
-        result.errorMessage = 'response format ID not recognized';
-        result.returnCode = LedgerError.DeviceIsBusy;
+        result.errorMessage = 'response format ID not recognized'
+        result.returnCode = LedgerError.DeviceIsBusy
       } else {
-        const appNameLen = response[1];
-        appName = response.slice(2, 2 + appNameLen).toString('ascii');
-        let idx = 2 + appNameLen;
-        const appVersionLen = response[idx];
-        idx += 1;
-        appVersion = response.slice(idx, idx + appVersionLen).toString('ascii');
-        idx += appVersionLen;
-        const appFlagsLen = response[idx];
-        idx += 1;
-        flagLen = appFlagsLen;
-        flagsValue = response[idx];
+        const appNameLen = response[1]
+        appName = response.slice(2, 2 + appNameLen).toString('ascii')
+        let idx = 2 + appNameLen
+        const appVersionLen = response[idx]
+        idx += 1
+        appVersion = response.slice(idx, idx + appVersionLen).toString('ascii')
+        idx += appVersionLen
+        const appFlagsLen = response[idx]
+        idx += 1
+        flagLen = appFlagsLen
+        flagsValue = response[idx]
       }
 
       return {
@@ -156,81 +151,78 @@ export default class InternetComputerApp {
         flagOnboarded: (flagsValue & 4) !== 0,
         // eslint-disable-next-line no-bitwise
         flagPINValidated: (flagsValue & 128) !== 0,
-      };
-    }, processErrorResponse);
+      }
+    }, processErrorResponse)
   }
 
   async deviceInfo(): Promise<ResponseDeviceInfo> {
-    return this.transport.send(0xe0, 0x01, 0, 0, Buffer.from([]), [0x6e00])
-      .then(response => {
-        const errorCodeData = response.slice(-2);
-        const returnCode = errorCodeData[0] * 256 + errorCodeData[1];
+    return this.transport.send(0xe0, 0x01, 0, 0, Buffer.from([]), [0x6e00]).then(response => {
+      const errorCodeData = response.slice(-2)
+      const returnCode = errorCodeData[0] * 256 + errorCodeData[1]
 
-        if (returnCode === 0x6e00) {
-          return {
-            return_code: returnCode,
-            error_message: "This command is only available in the Dashboard",
-          };
-        }
-
-        const targetId = response.slice(0, 4).toString("hex");
-
-        let pos = 4;
-        const secureElementVersionLen = response[pos];
-        pos += 1;
-        const seVersion = response.slice(pos, pos + secureElementVersionLen).toString();
-        pos += secureElementVersionLen;
-
-        const flagsLen = response[pos];
-        pos += 1;
-        const flag = response.slice(pos, pos + flagsLen).toString("hex");
-        pos += flagsLen;
-
-        const mcuVersionLen = response[pos];
-        pos += 1;
-        // Patch issue in mcu version
-        let tmp = response.slice(pos, pos + mcuVersionLen);
-        if (tmp[mcuVersionLen - 1] === 0) {
-          tmp = response.slice(pos, pos + mcuVersionLen - 1);
-        }
-        const mcuVersion = tmp.toString();
-
+      if (returnCode === 0x6e00) {
         return {
-          returnCode: returnCode,
-          errorMessage: errorCodeToString(returnCode),
-          // //
-          targetId,
-          seVersion,
-          flag,
-          mcuVersion,
-        };
-      }, processErrorResponse);
+          return_code: returnCode,
+          error_message: 'This command is only available in the Dashboard',
+        }
+      }
+
+      const targetId = response.slice(0, 4).toString('hex')
+
+      let pos = 4
+      const secureElementVersionLen = response[pos]
+      pos += 1
+      const seVersion = response.slice(pos, pos + secureElementVersionLen).toString()
+      pos += secureElementVersionLen
+
+      const flagsLen = response[pos]
+      pos += 1
+      const flag = response.slice(pos, pos + flagsLen).toString('hex')
+      pos += flagsLen
+
+      const mcuVersionLen = response[pos]
+      pos += 1
+      // Patch issue in mcu version
+      let tmp = response.slice(pos, pos + mcuVersionLen)
+      if (tmp[mcuVersionLen - 1] === 0) {
+        tmp = response.slice(pos, pos + mcuVersionLen - 1)
+      }
+      const mcuVersion = tmp.toString()
+
+      return {
+        returnCode: returnCode,
+        errorMessage: errorCodeToString(returnCode),
+        // //
+        targetId,
+        seVersion,
+        flag,
+        mcuVersion,
+      }
+    }, processErrorResponse)
   }
 
   async getAddressAndPubKey(path: string): Promise<ResponseAddress> {
-    const serializedPath = serializePath(path);
+    const serializedPath = serializePath(path)
     return this.transport
       .send(CLA, INS.GET_ADDR_SECP256K1, P1_VALUES.ONLY_RETRIEVE, 0, serializedPath, [0x9000])
-      .then(processGetAddrResponse, processErrorResponse);
+      .then(processGetAddrResponse, processErrorResponse)
   }
 
   async showAddressAndPubKey(path: string): Promise<ResponseAddress> {
-    const serializedPath = serializePath(path);
+    const serializedPath = serializePath(path)
 
     return this.transport
-      .send(CLA, INS.GET_ADDR_SECP256K1, P1_VALUES.SHOW_ADDRESS_IN_DEVICE, 0, serializedPath, [
-        LedgerError.NoErrors,
-      ])
-      .then(processGetAddrResponse, processErrorResponse);
+      .send(CLA, INS.GET_ADDR_SECP256K1, P1_VALUES.SHOW_ADDRESS_IN_DEVICE, 0, serializedPath, [LedgerError.NoErrors])
+      .then(processGetAddrResponse, processErrorResponse)
   }
 
   async signSendChunk(chunkIdx: number, chunkNum: number, chunk: Buffer, txtype: number): Promise<ResponseSign> {
-    let payloadType = PAYLOAD_TYPE.ADD;
+    let payloadType = PAYLOAD_TYPE.ADD
     if (chunkIdx === 1) {
-      payloadType = PAYLOAD_TYPE.INIT;
+      payloadType = PAYLOAD_TYPE.INIT
     }
     if (chunkIdx === chunkNum) {
-      payloadType = PAYLOAD_TYPE.LAST;
+      payloadType = PAYLOAD_TYPE.LAST
     }
 
     return this.transport
@@ -238,44 +230,43 @@ export default class InternetComputerApp {
         LedgerError.NoErrors,
         LedgerError.DataIsInvalid,
         LedgerError.BadKeyHandle,
-        LedgerError.SignVerifyError
+        LedgerError.SignVerifyError,
       ])
       .then((response: Buffer) => {
-        const errorCodeData = response.slice(-2);
-        const returnCode = errorCodeData[0] * 256 + errorCodeData[1];
-        let errorMessage = errorCodeToString(returnCode);
+        const errorCodeData = response.slice(-2)
+        const returnCode = errorCodeData[0] * 256 + errorCodeData[1]
+        let errorMessage = errorCodeToString(returnCode)
 
-        let preSignHash = Buffer.alloc(0);
-        let signatureRS = Buffer.alloc(0);
-        let signatureDER = Buffer.alloc(0);
+        let preSignHash = Buffer.alloc(0)
+        let signatureRS = Buffer.alloc(0)
+        let signatureDER = Buffer.alloc(0)
 
-        if (returnCode === LedgerError.BadKeyHandle ||
+        if (
+          returnCode === LedgerError.BadKeyHandle ||
           returnCode === LedgerError.DataIsInvalid ||
-          returnCode === LedgerError.SignVerifyError) {
-          errorMessage = `${errorMessage} : ${response
-            .slice(0, response.length - 2)
-            .toString('ascii')}`;
+          returnCode === LedgerError.SignVerifyError
+        ) {
+          errorMessage = `${errorMessage} : ${response.slice(0, response.length - 2).toString('ascii')}`
         }
 
         if (returnCode === LedgerError.NoErrors && response.length > 2) {
-          preSignHash = response.slice(0, PREHASH_LEN);
-          signatureRS = response.slice(PREHASH_LEN, PREHASH_LEN + SIGRSLEN);
-          signatureDER = response.slice(PREHASH_LEN + SIGRSLEN + 1, response.length - 2);
+          preSignHash = response.slice(0, PREHASH_LEN)
+          signatureRS = response.slice(PREHASH_LEN, PREHASH_LEN + SIGRSLEN)
+          signatureDER = response.slice(PREHASH_LEN + SIGRSLEN + 1, response.length - 2)
           return {
             preSignHash,
             signatureRS,
             signatureDER,
             returnCode: returnCode,
             errorMessage: errorMessage,
-          };
+          }
         }
 
         return {
           returnCode: returnCode,
           errorMessage: errorMessage,
-        } as ResponseSign;
-
-      }, processErrorResponse);
+        } as ResponseSign
+      }, processErrorResponse)
   }
 
   async sign(path: string, message: Buffer, txtype: number) {
@@ -287,83 +278,82 @@ export default class InternetComputerApp {
           preSignHash: null as null | Buffer,
           signatureRS: null as null | Buffer,
           signatureDER: null as null | Buffer,
-        } as ResponseSign;
+        } as ResponseSign
 
         for (let i = 1; i < chunks.length; i += 1) {
           // eslint-disable-next-line no-await-in-loop
-          result = await this.signSendChunk(1 + i, chunks.length, chunks[i], txtype % 256);
+          result = await this.signSendChunk(1 + i, chunks.length, chunks[i], txtype % 256)
           if (result.returnCode !== LedgerError.NoErrors) {
-            break;
+            break
           }
         }
-        return result;
-      }, processErrorResponse);
-    }, processErrorResponse);
+        return result
+      }, processErrorResponse)
+    }, processErrorResponse)
   }
 
   async signSendChunkUpdateCall(chunkIdx: number, chunkNum: number, chunk: Buffer, txtype: number): Promise<ResponseSignUpdateCall> {
-    let payloadType = PAYLOAD_TYPE.ADD;
+    let payloadType = PAYLOAD_TYPE.ADD
     if (chunkIdx === 1) {
-      payloadType = PAYLOAD_TYPE.INIT;
+      payloadType = PAYLOAD_TYPE.INIT
     }
     if (chunkIdx === chunkNum) {
-      payloadType = PAYLOAD_TYPE.LAST;
+      payloadType = PAYLOAD_TYPE.LAST
     }
 
     return this.transport
-        .send(CLA, INS.SIGN_COMBINED, payloadType, txtype, chunk, [
-          LedgerError.NoErrors,
-          LedgerError.DataIsInvalid,
-          LedgerError.BadKeyHandle,
-          LedgerError.SignVerifyError
-        ])
-        .then((response: Buffer) => {
-          const errorCodeData = response.slice(-2);
-          const returnCode = errorCodeData[0] * 256 + errorCodeData[1];
-          let errorMessage = errorCodeToString(returnCode);
+      .send(CLA, INS.SIGN_COMBINED, payloadType, txtype, chunk, [
+        LedgerError.NoErrors,
+        LedgerError.DataIsInvalid,
+        LedgerError.BadKeyHandle,
+        LedgerError.SignVerifyError,
+      ])
+      .then((response: Buffer) => {
+        const errorCodeData = response.slice(-2)
+        const returnCode = errorCodeData[0] * 256 + errorCodeData[1]
+        let errorMessage = errorCodeToString(returnCode)
 
-          let RequestHash = Buffer.alloc(0);
-          let RequestSignatureRS = Buffer.alloc(0);
-          let StatusReadHash = Buffer.alloc(0);
-          let StatusReadSignatureRS = Buffer.alloc(0);
+        let RequestHash = Buffer.alloc(0)
+        let RequestSignatureRS = Buffer.alloc(0)
+        let StatusReadHash = Buffer.alloc(0)
+        let StatusReadSignatureRS = Buffer.alloc(0)
 
-          if (returnCode === LedgerError.BadKeyHandle ||
-              returnCode === LedgerError.DataIsInvalid ||
-              returnCode === LedgerError.SignVerifyError) {
-            errorMessage = `${errorMessage} : ${response
-                .slice(0, response.length - 2)
-                .toString('ascii')}`;
-          }
+        if (
+          returnCode === LedgerError.BadKeyHandle ||
+          returnCode === LedgerError.DataIsInvalid ||
+          returnCode === LedgerError.SignVerifyError
+        ) {
+          errorMessage = `${errorMessage} : ${response.slice(0, response.length - 2).toString('ascii')}`
+        }
 
-          if (returnCode === LedgerError.NoErrors && response.length > 2) {
-            RequestHash = response.slice(0, 32);
-            RequestSignatureRS = response.slice(32, 96);
-            StatusReadHash = response.slice(96, 128);
-            StatusReadSignatureRS = response.slice(128, 192);
-            return {
-              RequestHash,
-              RequestSignatureRS,
-              StatusReadHash,
-              StatusReadSignatureRS,
-              returnCode: returnCode,
-              errorMessage: errorMessage,
-            };
-          }
-
+        if (returnCode === LedgerError.NoErrors && response.length > 2) {
+          RequestHash = response.slice(0, 32)
+          RequestSignatureRS = response.slice(32, 96)
+          StatusReadHash = response.slice(96, 128)
+          StatusReadSignatureRS = response.slice(128, 192)
           return {
+            RequestHash,
+            RequestSignatureRS,
+            StatusReadHash,
+            StatusReadSignatureRS,
             returnCode: returnCode,
             errorMessage: errorMessage,
-          } as ResponseSignUpdateCall;
+          }
+        }
 
-        }, processErrorResponse);
+        return {
+          returnCode: returnCode,
+          errorMessage: errorMessage,
+        } as ResponseSignUpdateCall
+      }, processErrorResponse)
   }
 
   async signUpdateCall(path: string, request: Buffer, checkStatus: Buffer, txtype: number) {
-    const message = Buffer.alloc(8 + request.byteLength + checkStatus.byteLength);
-    message.writeUInt32LE(checkStatus.byteLength, 0);
-    checkStatus.copy(message, 4);
-    message.writeUInt32LE(request.byteLength, 4 + checkStatus.byteLength);
-    request.copy(message, 8 + checkStatus.byteLength);
+    const message = Buffer.alloc(8 + request.byteLength + checkStatus.byteLength)
+    message.writeUInt32LE(checkStatus.byteLength, 0)
+    checkStatus.copy(message, 4)
+    message.writeUInt32LE(request.byteLength, 4 + checkStatus.byteLength)
+    request.copy(message, 8 + checkStatus.byteLength)
     console.log(message.toString('hex'))
     return this.signGetChunks(path, message).then(chunks => {
       return this.signSendChunk(1, chunks.length, chunks[0], txtype % 256).then(async response => {
@@ -374,17 +364,17 @@ export default class InternetComputerApp {
           RequestSignatureRS: null as null | Buffer,
           StatusReadHash: null as null | Buffer,
           StatusReadSignatureRS: null as null | Buffer,
-        } as ResponseSignUpdateCall;
+        } as ResponseSignUpdateCall
 
         for (let i = 1; i < chunks.length; i += 1) {
           // eslint-disable-next-line no-await-in-loop
-          result = await this.signSendChunkUpdateCall(1 + i, chunks.length, chunks[i], txtype % 256);
+          result = await this.signSendChunkUpdateCall(1 + i, chunks.length, chunks[i], txtype % 256)
           if (result.returnCode !== LedgerError.NoErrors) {
-            break;
+            break
           }
         }
-        return result;
-      }, processErrorResponse);
-    }, processErrorResponse);
+        return result
+      }, processErrorResponse)
+    }, processErrorResponse)
   }
 }
--- js/src/types.ts
+++ js/src/types.ts
@@ -1,53 +1,53 @@
-import {LedgerError} from "./common";
+import { LedgerError } from './common'
 
 export interface ResponseBase {
-  errorMessage?: string;
-  returnCode: LedgerError;
+  errorMessage?: string
+  returnCode: LedgerError
 }
 
 export interface ResponseAddress extends ResponseBase {
-  publicKey: Buffer;
-  principal: Buffer;
-  address: Buffer;
-  principalText: string;
+  publicKey: Buffer
+  principal: Buffer
+  address: Buffer
+  principalText: string
 }
 
 export interface ResponseVersion extends ResponseBase {
-  testMode: boolean;
-  major: number;
-  minor: number;
-  patch: number;
-  deviceLocked: boolean;
-  targetId: string;
+  testMode: boolean
+  major: number
+  minor: number
+  patch: number
+  deviceLocked: boolean
+  targetId: string
 }
 
 export interface ResponseAppInfo extends ResponseBase {
-  appName: string;
-  appVersion: string;
-  flagLen: number;
-  flagsValue: number;
-  flagRecovery: boolean;
-  flagSignedMcuCode: boolean;
-  flagOnboarded: boolean;
-  flagPINValidated: boolean;
+  appName: string
+  appVersion: string
+  flagLen: number
+  flagsValue: number
+  flagRecovery: boolean
+  flagSignedMcuCode: boolean
+  flagOnboarded: boolean
+  flagPINValidated: boolean
 }
 
 export interface ResponseDeviceInfo extends ResponseBase {
-  targetId: string;
-  seVersion: string;
-  flag: string;
-  mcuVersion: string;
+  targetId: string
+  seVersion: string
+  flag: string
+  mcuVersion: string
 }
 
 export interface ResponseSign extends ResponseBase {
-  preSignHash: Buffer,
-  signatureRS: Buffer,
-  signatureDER: Buffer,
+  preSignHash: Buffer
+  signatureRS: Buffer
+  signatureDER: Buffer
 }
 
 export interface ResponseSignUpdateCall extends ResponseBase {
-  RequestHash: Buffer,
-  RequestSignatureRS: Buffer,
-  StatusReadHash: Buffer,
-  StatusReadSignatureRS: Buffer,
+  RequestHash: Buffer
+  RequestSignatureRS: Buffer
+  StatusReadHash: Buffer
+  StatusReadSignatureRS: Buffer
 }
