a7a5e5bfc40abe1753b80aa63dea20449ca7023e
neithanmo <neithanmo@gmail.com>
2024-03-19 06:52:54 -0600

    move unit_test to fill_address

--- app/src/common/actions.h
+++ app/src/common/actions.h
@@ -22,6 +22,8 @@
 #include <os_io_seproxyhal.h>
 #include "coin.h"
 #include "zxerror.h"
+// TODO: remove later
+#include "rslib.h"
 
 extern uint16_t action_addrResponseLen;
 
@@ -56,6 +58,33 @@ __Z_INLINE void app_sign_combined() {
 }
 
 __Z_INLINE zxerr_t app_fill_address() {
+    // Assuming the signature is 48 bytes long based on the provided values
+    uint8_t s[48] = {
+        139, 118, 54, 85, 203, 37, 178, 69, 140, 15, 123, 156, 250, 54, 205, 107,
+        22, 254, 170, 98, 234, 151, 252, 248, 245, 75, 211, 209, 237, 75, 135, 119,
+        53, 244, 174, 38, 241, 127, 34, 154, 2, 92, 174, 10, 73, 110, 128, 24
+    };
+
+    // Assuming the public key is 96 bytes long based on the provided values
+    uint8_t pub[96] = {
+        136, 241, 121, 119, 242, 65, 192, 110, 129, 119, 65, 77, 158, 13, 150, 144,
+        28, 235, 33, 208, 173, 221, 78, 19, 60, 123, 224, 65, 6, 100, 121, 203,
+        211, 101, 20, 169, 44, 125, 233, 145, 41, 91, 200, 233, 176, 158, 87, 101,
+        14, 124, 251, 239, 197, 63, 193, 29, 63, 169, 173, 27, 106, 244, 66, 35,
+        18, 131, 154, 12, 85, 56, 162, 240, 100, 125, 155, 115, 241, 135, 95, 223,
+        191, 44, 141, 140, 9, 202, 43, 152, 228, 117, 44, 46, 126, 194, 128, 157
+    };
+
+    // Assuming the message is 11 bytes long based on the provided values
+    uint8_t message[11] = {
+        104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100
+    };
+    zemu_log_stack("\n calling verify_bls_sign!!!***\n");
+    verify_bls_sign(s, message, 11, pub);
+    zemu_log_stack("\nverify_bls_sign_done!!!***\n");
+
+    CHECK_APP_CANARY();
+
     // Put data directly in the apdu buffer
     MEMZERO(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE);
 
