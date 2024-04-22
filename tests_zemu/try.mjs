347a8f19c3bd97a2edcedcaefe305cc8cfa2e726
neithanmo <neithanmo@gmail.com>
2024-04-22 13:58:47 -0600

    add debug code to test on device

---
+++ tests_zemu/try.mjs
@@ -0,0 +1,27 @@
+import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
+import ledger_logs from '@ledgerhq/logs'
+import AvalancheApp from '@zondax/ledger-icp'
+
+const APP_DERIVATION = "m/44'/223'/0'/0/0"
+
+async function get_icp_address(app) {
+  // const resp = await app.getAddressAndPubKey(APP_DERIVATION, true)
+  const resp = await app.getAddressAndPubKey(APP_DERIVATION)
+
+  console.log(resp)
+}
+
+async function main() {
+  const transport = await TransportNodeHid.default.open()
+  ledger_logs.listen(log => {
+    console.log(`${log.type} ${log.message}`)
+  })
+  const app = new AvalancheApp.default(transport)
+
+  const resp = await get_icp_address(app)
+  console.log(resp)
+}
+
+;(async () => {
+  await main()
+})()
