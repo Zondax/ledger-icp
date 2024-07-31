f5f1bf826d2eb90ac6959aff3c494e798ccb9538
Carlos Medeiros <carlos.medeiros@zondax.ch>
2024-07-31 13:48:25 +0100

    adding new nvdata functions to save bls info

--- app/src/nvdata.c
+++ app/src/nvdata.c
@@ -17,24 +17,88 @@
 #include "nvdata.h"
 
 #include "app_main.h"
+#include "cx.h"
+#include "os.h"
+#include "view.h"
 
-transaction_header_t transaction_header;
+bls_data_t NV_CONST
+N_bls_data_impl __attribute__ ((aligned(64)));
+#define N_bls_data (*(NV_VOLATILE bls_data_t *)PIC(&N_bls_data_impl))
 
+bls_header_t bls_header;
+
+// Save data
+zxerr_t save_consent_request(uint8_t* data, uint16_t data_len) {
+    if (data_len == 0 || data_len > MAX_DATA_SIZE) {
+        return zxerr_out_of_bounds;
+    }
+
+    bls_header.consent_request_len = data_len;
+    MEMCPY_NV((void *)&N_bls_data.consent_request, data, data_len);
+    return zxerr_ok;
+}
+
+zxerr_t save_canister_call(uint8_t* data, uint16_t data_len) {
+    if (data_len == 0 || data_len > MAX_DATA_SIZE) {
+        return zxerr_out_of_bounds;
+    }
+
+    bls_header.canister_call_len = data_len;
+    MEMCPY_NV((void *)&N_bls_data.canister_call, data, data_len);
+    return zxerr_ok;
+}
+
+zxerr_t save_root_key(uint8_t* data, uint16_t data_len) {
+    if (data_len == 0 || data_len != ROOT_KEY_LEN) {
+        return zxerr_out_of_bounds;
+    }
+
+    MEMCPY_NV((void *)&N_bls_data.root_key, data, data_len);
+    return zxerr_ok;
+}
+
+// Retrieve data
+uint8_t *get_consent_request() {
+    return (uint8_t *)&N_bls_data.consent_request;
+}
+
+uint8_t *get_canister_call() {
+    return (uint8_t *)&N_bls_data.canister_call;
+}
+
+uint8_t *get_root_key() {
+    return (uint8_t *)&N_bls_data.root_key;
+}
+
+uint8_t get_consent_request_length(void) {
+    return bls_header.consent_request_len;
+}
+
+uint8_t get_canister_call_length(void) {
+    return bls_header.canister_call_len;
+}
 
 // STATE
 uint8_t get_state() {
-    return transaction_header.state;
+    return bls_header.state;
 }
 
 void set_state(uint8_t state) {
-    transaction_header.state = state;
+    bls_header.state = state;
 }
 
 void state_reset() {
-    transaction_header.state = STATE_INITIAL;
+    bls_header.state = STATE_INITIAL;
 }
 
+void zeroize_data(){
+    uint8_t tmp[MAX_DATA_SIZE] = {0};
+    MEMCPY_NV((void *)&N_bls_data.consent_request, tmp, MAX_DATA_SIZE);
+    MEMCPY_NV((void *)&N_bls_data.canister_call, tmp, MAX_DATA_SIZE);
+    MEMCPY_NV((void *)&N_bls_data.root_key, tmp, ROOT_KEY_LEN);
+}
 
 void bls_nvm_reset() {
-    MEMZERO(&transaction_header, sizeof(transaction_header_t));
+    MEMZERO(&bls_header, sizeof(bls_header_t));
+    zeroize_data();
 }
