282363386fb1771374c5d7fbc16d5dbe77d10160
neithanmo <neithanmo@gmail.com>
2024-12-11 16:45:23 +0100

    Use target to define constants

---
+++ app/rust/build.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let target = std::env::var("RUST_TARGET_NAME").unwrap_or_default();
+    match target.as_str() {
+        "TARGET_STAX" => println!("cargo:rustc-cfg=device_stax"),
+        "TARGET_FLEX" => println!("cargo:rustc-cfg=device_flex"),
+        "TARGET_NANOS2" => println!("cargo:rustc-cfg=device_nanos2"),
+        "TARGET_NANOX" => println!("cargo:rustc-cfg=device_nanox"),
+        _ => println!("cargo:rustc-cfg=device_nanos"), // default
+    }
+}
