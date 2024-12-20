6692d12873e23788d09a3abb37f5e96fa101882f
neithanmo <neithanmo@gmail.com>
2024-12-20 09:21:53 -0600

    Export heartbeat and log functions

--- app/rust/src/lib.rs
+++ app/rust/src/lib.rs
@@ -35,7 +35,7 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 pub fn zlog(_msg: &str) {
-    #[cfg(not(test))]
+    #[cfg(all(not(test), not(feature = "clippy"), not(feature = "fuzzing")))]
     unsafe {
         zemu_log_stack(_msg.as_bytes().as_ptr());
     }
@@ -43,9 +43,9 @@ pub fn zlog(_msg: &str) {
     std::println!("{}", _msg);
 }
 
-#[cfg(not(test))]
+#[cfg(all(not(test), not(feature = "clippy"), not(feature = "fuzzing")))]
 pub fn check_canary() {
-    unsafe { check_app_canary() }
+    unsafe { _check_canary() }
 }
 
 #[cfg(test)]
@@ -53,5 +53,29 @@ pub fn check_canary() {}
 
 extern "C" {
     fn zemu_log_stack(s: *const u8);
-    fn check_app_canary();
+    fn _check_canary();
+    fn log_number(e: *const u8, number: u32);
+}
+
+#[cfg(all(not(test), not(feature = "clippy"), not(feature = "fuzzing")))]
+extern "C" {
+    fn io_heartbeat();
+}
+
+// Lets the device breath between computations
+pub(crate) fn heartbeat() {
+    #[cfg(all(not(test), not(feature = "clippy"), not(feature = "fuzzing")))]
+    unsafe {
+        io_heartbeat()
+    }
+}
+
+// Lets the device breath between computations
+pub(crate) fn log_num(s: &str, number: u32) {
+    #[cfg(all(not(test), not(feature = "clippy"), not(feature = "fuzzing")))]
+    unsafe {
+        log_number(s.as_bytes().as_ptr(), number);
+    }
+    #[cfg(test)]
+    std::println!("{s}: {number}");
 }
