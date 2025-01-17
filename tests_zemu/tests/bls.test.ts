9b0bf5f845d9e23cc659c886c0561ff11696f8d5
neithanmo <neithanmo@gmail.com>
2025-01-17 11:42:00 -0600

    Enable Bls test and its snappshots

--- tests_zemu/tests/bls.test.ts
+++ tests_zemu/tests/bls.test.ts
@@ -20,8 +20,7 @@ import { DEFAULT_OPTIONS, DEVICE_MODELS_BLS } from './common'
 
 jest.setTimeout(180000)
 
-// describe.skip('Bls', function () {
-describe.skip('Bls', function () {
+describe('Bls', function () {
   test.concurrent.each(DEVICE_MODELS_BLS)('verify_with_default_key', async function (m) {
     const sim = new Zemu(m.path)
     try {
