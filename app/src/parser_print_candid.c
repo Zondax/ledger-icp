5c1e7ef1c1ecbc9a5c99cddb8b341126f3ece8c0
neithanmo <neithanmo@gmail.com>
2025-02-11 11:41:49 -0600

    Fix token display title and update test vector(fixed)

--- app/src/parser_print_candid.c
+++ app/src/parser_print_candid.c
@@ -1110,8 +1110,14 @@ parser_getItemICRCTransfer(uint8_t displayIdx, char *outKey, uint16_t outKeyLen,
       snprintf(outKey, outKeyLen, "Transaction type");
       snprintf(outVal, outValLen, is_stake_tx ? "Stake Neuron" : "Send ICP");
     } else {
-      snprintf(outKey, outKeyLen, "Transaction type");
-      snprintf(outVal, outValLen, "Send Tokens");
+      bool found = token != NULL;
+      if (token != NULL) {
+        snprintf(outKey, outKeyLen, "Transaction type");
+        snprintf(outVal, outValLen, "Send %s", token->token_symbol);
+      } else {
+        snprintf(outKey, outKeyLen, "Transaction type");
+        snprintf(outVal, outValLen, "Send Tokens");
+      }
     }
     return parser_ok;
   }
