ab71fc01e042c581fa3bad7e85d7afbadcf040ef
Andrés Benso <andresbenso@gmail.com>
2025-10-07 10:20:10 -0300

    Add apex support (#313)
    * add snapshots
    
    * update toml file
    
    * bump version
    
    * update zemu
    
    * update icon

--- app/rust/build.rs
+++ app/rust/build.rs
@@ -3,6 +3,7 @@ fn main() {
     match target.as_str() {
         "TARGET_STAX" => println!("cargo:rustc-cfg=device_stax"),
         "TARGET_FLEX" => println!("cargo:rustc-cfg=device_flex"),
+        "TARGET_APEX_P" => println!("cargo:rustc-cfg=device_apex_p"),
         "TARGET_NANOS2" => println!("cargo:rustc-cfg=device_nanos2"),
         "TARGET_NANOX" => println!("cargo:rustc-cfg=device_nanox"),
         _ => println!("cargo:rustc-cfg=device_nanos"), // default
--- app/src/parser_print_helper.c
+++ app/src/parser_print_helper.c
@@ -26,7 +26,7 @@
 #define ICP_YEAR_IN_SECONDS (uint64_t)(ICP_DAY_IN_SECONDS * 365.25)
 #define MAX_OUTPUT_LEN 35
 
-#if defined(TARGET_STAX) || defined(TARGET_FLEX)
+#if defined(TARGET_STAX) || defined(TARGET_FLEX) || defined(TARGET_APEX_P)
 #include "view_internal.h"
 const uint8_t LINES_PER_PAGE = MAX_LINES_PER_PAGE_REVIEW;
 static const char SEPARATOR = 0x0a;  // newline
@@ -287,7 +287,7 @@ parser_error_t page_principal_with_subaccount(const uint8_t *sender, uint16_t se
     *(text_ptr + crcLen) = SEPARATOR;
     text_ptr += crcLen + 1;
 
-#if !defined(TARGET_STAX) && !defined(TARGET_FLEX)  // needed if crc32 length is < 7
+#if !defined(TARGET_STAX) && !defined(TARGET_FLEX) && !defined(TARGET_APEX_P)  // needed if crc32 length is < 7
     for (uint8_t i = crcLen; i < 7; i++) {
         *text_ptr = ' ';
         text_ptr++;
